9a309f7b5f1dc6891f495b8360f5c0fc8e7b8439
Shawn Cooper <scoop1120@gmail.com>
2025-02-07 01:18:21 -0800

    Update normalized_query.ex

--- lib/mongo_ecto/normalized_query.ex
+++ lib/mongo_ecto/normalized_query.ex
@@ -768,20 +768,35 @@ defmodule Mongo.Ecto.NormalizedQuery do
   end
 
   defp order(%Query{order_bys: order_bys} = query, {_coll, _model, pk}) do
+    IO.puts("Running order function")
+
     order_bys
-    |> Enum.flat_map(fn %Query.QueryExpr{expr: expr} ->
-      Enum.map(expr, &order_by_expr(&1, pk, query))
+    |> Enum.flat_map(fn
+      %Query.QueryExpr{expr: expr} ->
+        Enum.map(expr, &order_by_expr(&1, pk, query))
+
+      %Query.ByExpr{expr: expr} ->
+        Enum.map(expr, &order_by_expr(&1, pk, query))
     end)
     |> map_unless_empty
   end
 
   defp command(:update, %Query{updates: updates} = query, params, {_coll, _model, pk}) do
+    IO.puts("Running command function")
+
     updates
-    |> Enum.flat_map(fn %Query.QueryExpr{expr: expr} ->
-      Enum.map(expr, fn {key, value} ->
-        value = value |> value(params, pk, query, "update clause")
-        {update_op(key, query), value}
-      end)
+    |> Enum.flat_map(fn
+      %Query.QueryExpr{expr: expr} ->
+        Enum.map(expr, fn {key, value} ->
+          value = value |> value(params, pk, query, "update clause")
+          {update_op(key, query), value}
+        end)
+
+      %Query.ByExpr{expr: expr} ->
+        Enum.map(expr, fn {key, value} ->
+          value = value |> value(params, pk, query, "update clause")
+          {update_op(key, query), value}
+        end)
     end)
     |> merge_keys(query, "update clause")
   end
